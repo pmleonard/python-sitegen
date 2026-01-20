ab9d5ea14dfafe036e09e1548e5c99c78382d36b
pmleonard <pmleonard@users.noreply.github.com>
2026-01-19 21:20:22 -0500

    Update main.py

--- src/main.py
+++ src/main.py
@@ -275,11 +275,10 @@ def main():
 
     print("\nSite generation complete!")
 
-
-    # if os.path.isdir(output_directory):
-    #     ghp_import(srcdir=output_directory, push=True, force=True, no_history=True)
-    # else:
-    #     print(f"Error: {output_directory} does not exist")
+    if os.path.isdir(output_directory):
+        ghp_import(srcdir=output_directory, push=True, force=True, no_history=True)
+    else:
+        print(f"Error: {output_directory} does not exist")
 
 if __name__ == '__main__':
     main()
