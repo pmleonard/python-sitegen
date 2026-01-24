5d169907c758cdbb81e7c0ea0ebe48e306c703fb
pmleonard <pmleonard@users.noreply.github.com>
2026-01-24 13:28:21 -0500

    rename menu id

--- _data/assets/scripts/script.js
+++ _data/assets/scripts/script.js
@@ -1,6 +1,6 @@
 function toggleMenu() {
   const hamburger = document.querySelector(".hamburger");
-  const menu = document.getElementById("menu");
+  const menu = document.getElementById("hmbgr_menu");
 
   hamburger.classList.toggle("change");
   menu.classList.toggle("menu-visible");
